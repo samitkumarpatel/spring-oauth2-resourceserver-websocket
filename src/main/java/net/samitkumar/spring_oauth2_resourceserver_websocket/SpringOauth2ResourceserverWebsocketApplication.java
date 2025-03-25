3d1b8a69c499ad36de0b74bd715ef18ebe6828ea
samitkumarpatel <samitkumarpatel@gmail.com>
2025-03-25 12:47:33 +0000

    first commit

---
+++ src/main/java/net/samitkumar/spring_oauth2_resourceserver_websocket/SpringOauth2ResourceserverWebsocketApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.spring_oauth2_resourceserver_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ResourceserverWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ResourceserverWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/spring_oauth2_resourceserver_websocket/SpringOauth2ResourceserverWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.spring_oauth2_resourceserver_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceserverWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
